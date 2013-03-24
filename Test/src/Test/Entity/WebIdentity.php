089911d7d8423df8837c21fc975367c516213806
Deschamps <beroots74@gmail.com>
2013-03-24 18:57:37 +0100

    update

--- Test/src/Test/Entity/WebIdentity.php
+++ Test/src/Test/Entity/WebIdentity.php
@@ -12,4 +12,58 @@ class WebIdentity
 
     /** @var string Sender's email adress */
     public $email;
+
+    /**
+     * @param string $name
+     * @return Name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $firstname
+     * @return Firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $email
+     * @return Email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
 }
