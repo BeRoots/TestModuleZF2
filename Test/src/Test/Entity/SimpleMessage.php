24e3a003721b0976e37761b6be662e0533d29e04
Deschamps <beroots74@gmail.com>
2013-03-24 18:57:29 +0100

    update

--- Test/src/Test/Entity/SimpleMessage.php
+++ Test/src/Test/Entity/SimpleMessage.php
@@ -12,4 +12,58 @@ class SimpleMessage
 
     /** @var int Send message's copy to sender */
     public $copy;
+
+    /**
+     * @param string $subject
+     * @return Subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $message
+     * @return Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $copy
+     * @return Copy
+     */
+    public function setCopy($copy)
+    {
+        $this->copy = $copy;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCopy()
+    {
+        return $this->copy;
+    }
 }
